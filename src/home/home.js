45408d617a61e3dbbeb6bf09aa7e0497abc1a967
William <wmayhood@outlook.com>
2024-10-02 11:06:39 -0400

    Refactored the home.js code to follow a component design.  The js code now creates sectionsof the website individually, and can be reused to create as many sections as needed

--- src/home/home.js
+++ src/home/home.js
@@ -3,26 +3,45 @@ import {createElement} from "../helperFunction.js"
 import californiaRoll from "../images/california-roll.jpg"
 import "./style.css"
 
-const generateContainer = (title, text) =>{
 
+const generateComponent = ({title, text, buttons, imageSrc}) =>{
+
+    const section = createElement({type: 'section', className: 'sectionComponent'})
 
     let infoContainer = createElement({type: 'div', className: 'infoContainer'});
 
     const h1 = createElement({type: 'h1', className: 'home-title', textContent: title});
 
-    const p = createElement({type: 'p', 
-        
-        textContent : text
-
-    });
+    const p = createElement({type: 'p', textContent : text});
 
     infoContainer.appendChild(h1);
     infoContainer.appendChild(p);
-    // infoContainer.appendChild(button);
 
-    // section.appendChild(img);
-return infoContainer;
+    if(buttons && buttons.length > 0){
+
+        buttons.forEach(button =>{
+            const btn = createElement({type: 'button', className: button.className, textContent:button.textContent});
+            infoContainer.appendChild(btn);
+        });
+
+
+    }
+
+    section.appendChild(infoContainer);
+    
+    //create image container
+    if(imageSrc){
+        const imageContainer = createImageDiv(imageSrc);
+        section.appendChild(imageContainer);
+    }
+    
+    
+    
+
+    return section;
+
 }
+
 
 const createImageDiv = (imageSrc) =>{
 
@@ -41,24 +60,27 @@ const displayHomePage = () =>{
 
     const contentContainer = document.querySelector('.content');
     contentContainer.innerHTML = "";
-    const section = createElement({type: 'section', className: 'sectionComponent'})
-    
+   
     ///first Section////
-    let title = "Fresh Sushi & BYOB";
+    const firstComponent = generateComponent({
 
-    let textContent = `Savor the flavor of our Grilled Salmon Kama, a unique delight now featured
+        title:"Fresh Sushi & BYOB",
+
+        text:`Savor the flavor of our Grilled Salmon Kama, a unique delight now featured
         on our All-You-Can-Eat menu. Available exclusively during Friday dinner and throughout the weekend—until sold out!
-        Keep in mind, that there is a limit of one serving per two guests.`;
+        Keep in mind, that there is a limit of one serving per two guests.`,
 
-    const infoContainer = generateContainer(title, textContent);
-    const button = createElement({type:'button', className: "reservation", textContent:"Make a Reservation"});
-    const buttonVarient = createElement({type:'button',  className: "orderButton", textContent: "Order Now"});
-    infoContainer.appendChild(button);
-    infoContainer.appendChild(buttonVarient);
-    const imageContainer = createImageDiv(californiaRoll);
-    section.appendChild(infoContainer);
-    section.appendChild(imageContainer);
-    contentContainer.appendChild(section);
+        buttons: [
+
+            {className: 'reservation', textContent:'Make a Reservation'},
+            {className: 'orderButton', textContent: 'Order Now'}
+
+        ],
+        imageSrc: californiaRoll
+
+    });
+    
+    contentContainer.appendChild(firstComponent);
 
 };
 
